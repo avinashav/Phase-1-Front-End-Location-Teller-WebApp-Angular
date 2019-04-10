1df4a94d404bfd75e5ee679994a67ae1a3059022
Avinash <avinash.softs@gmail.com>
2019-04-10 21:12:42 +0530

    added images

--- src/app/view/view/view.component.ts
+++ src/app/view/view/view.component.ts
@@ -15,7 +15,7 @@ export class ViewComponent implements OnInit {
   ngOnInit() {
   }
   LocationInfoAPICall() {
-    this.http.get('https://20190305t120915-dot-ipdetail.appspot.com/')
+    this.http.get('http://10.12.10.134:8080/ip/')
     .subscribe((response) => {
       this.response = response;
       console.log(this.response);
